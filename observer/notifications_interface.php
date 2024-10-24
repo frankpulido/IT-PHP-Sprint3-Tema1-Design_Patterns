b73c823ae5d99df8692c1b9b6f92a3f7ad43c323
frankpulido <frankpulido@me.com>
2024-10-24 17:04:48 +0200

    notifications interface : eliminated troublesome method pushObserver

--- observer/notifications_interface.php
+++ observer/notifications_interface.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 require_once "taskupload.php";
 
 interface Notifications {
-    public function pushObserver();
+    //public function pushObserver();
     public function getNotifiedAboutUploadedTask(TaskUpload $task);
     public function getNotifiedAboutNewUserStudent();
 }
